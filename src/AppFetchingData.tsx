c4c0943ff486ae87ba632a973aeb7ff9da897db4
subrotoice <subroto.iu@gmail.com>
2023-05-14 09:05:10 +0600

    Showing Loader untill data come from server

--- src/AppFetchingData.tsx
+++ src/AppFetchingData.tsx
@@ -9,16 +9,23 @@ interface User {
 const AppFetchingData = () => {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
   useEffect(() => {
+    setLoading(true);
     const controller = new AbortController();
     axios
       .get<User[]>("https://jsonplaceholder.typicode.com/users", {
         signal: controller.signal,
       })
-      .then((res) => setUsers(res.data))
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return; // If no data here then
         setError(err.message);
+        setLoading(false);
       });
 
     return () => controller.abort();
@@ -26,6 +33,7 @@ const AppFetchingData = () => {
   return (
     <div>
       {error && <p className="text-danger">{error}</p>}
+      {isLoading && <div className="spinner-border"></div>}
       <ul>
         {users.map((user) => (
           <li>{user.name}</li>
